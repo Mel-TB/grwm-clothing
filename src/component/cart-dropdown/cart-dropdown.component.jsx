5268209950551e195a1c5ed5a9135405630712da
Mel-TB <melinda.trami@live.fr>
2023-06-05 16:17:23 +0200

    created checkout page and functionnality

--- src/component/cart-dropdown/cart-dropdown.component.jsx
+++ src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -1,24 +1,30 @@
 import { useContext } from 'react';
-import Button from '../button/button.component';
+import { useNavigate } from 'react-router-dom';
 
-import CartItem from '../cart-item/cart-item.component';
 import { CartContext } from '../../context/cart.context';
 
+import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
+
 import './cart-dropdown.styles.scss';
+
 const CartDropdown = () => {
-	const { cartItems } = useContext(CartContext);
+	const { cartItems, isCartOpen, setIsCartOpen } = useContext(CartContext);
+	const navigate = useNavigate();
+
+	const goToCheckoutHandler = () => {
+		navigate('/checkout');
+		setIsCartOpen(!isCartOpen);
+	};
+
 	return (
 		<div className="cart-dropdown-container">
 			<div className="cart-items">
-				{cartItems.length ? (
-					cartItems.map((cartItem) => (
-						<CartItem key={cartItem.id} cartItem={cartItem} />
-					))
-				) : (
-					<span>Your cart is empty </span>
-				)}
+				{cartItems.map((item) => (
+					<CartItem key={item.id} cartItem={item} />
+				))}
 			</div>
-			<Button buttonType="inverted">Check Out</Button>
+			<Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
 		</div>
 	);
 };
