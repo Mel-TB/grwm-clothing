ff81837c7593bf7d42045f98b668e29e4ceadbe5
Mel-TB <melinda.trami@live.fr>
2023-06-06 14:50:06 +0200

    added new collection to firestore

--- src/context/product.context.jsx
+++ src/context/product.context.jsx
@@ -1,12 +1,14 @@
 import { createContext, useState } from 'react';
-import PRODUCTS from '../shop-data.json';
+
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
 
 export const ProductContext = createContext({
 	products: [],
 });
 
 export const ProductProvider = ({ children }) => {
-	const [products, setProducts] = useState(PRODUCTS);
+	const [products, setProducts] = useState([]);
+
 	const value = { products };
 	return (
 		<ProductContext.Provider value={value}>{children}</ProductContext.Provider>
