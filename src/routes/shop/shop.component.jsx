02f549492a3828fbdcc604a74ffefb184342715c
Mel-TB <melinda.trami@live.fr>
2023-06-16 13:54:35 +0200

    updating thunk pattern for fetch categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,8 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils.js';
-import { setCategories } from '../../store/categories/category.action.js';
+import { fetchCategoriesAsync } from '../../store/categories/category.action.js';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
@@ -12,13 +11,7 @@ const Shop = () => {
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		const getCategoriesMap = async () => {
-			const categoriesArray = await getCategoriesAndDocuments('categories');
-
-			dispatch(setCategories(categoriesArray));
-		};
-
-		getCategoriesMap();
+		dispatch(fetchCategoriesAsync());
 	}, []);
 
 	return (
